d64de716cd155cf8342643c96b2b35e81ec45a52
Elik Skorobohatov <ee.sd.2005@gmail.com>
2024-06-14 16:06:13 +0300

    04/06

--- js/slider.js
+++ js/slider.js
@@ -4,12 +4,19 @@ const slideWight = document.querySelector(".slider");
 
 let position = 0;
 let slideWidth = document.querySelector('.block-port.video').offsetWidth;
-const maxPosition = -3 * 1080;
+let maxPosition = 2 * -slideWidth;
 
 
 
 nextSlideBtn.addEventListener("click", function() {
     slideWidth = document.querySelector('.block-port.video').offsetWidth;
+
+    if (window.innerWidth <= 900){
+        maxPosition = 5 * -slideWidth;
+    } else {
+        maxPosition = 2 * -slideWidth;
+    }
+
     if (position <= maxPosition) {
         position = 0;
     } else {
@@ -21,6 +28,12 @@ nextSlideBtn.addEventListener("click", function() {
 
 untilSlideBtn.addEventListener("click", function() {
     slideWidth = document.querySelector('.block-port.video').offsetWidth;
+    if (window.innerWidth <= 900){
+        maxPosition = 5 * -slideWidth;
+    } else {
+        maxPosition = 2 * -slideWidth;
+    }
+
     if (position === 0) {
         position = maxPosition;
     } else {
